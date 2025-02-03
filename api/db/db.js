fc7735b3f335a8ee607924c6fc7b90f48f03109a
me-Akshay <rishubxr@gmail.com>
2025-02-03 08:01:54 +0530

    atlas connection done

--- api/db/db.js
+++ api/db/db.js
@@ -3,11 +3,25 @@
 
 const mongoose=require('mongoose')
 
+//local
+
+// const connectToDb =()=>{
+
+    
+//   mongoose.connect("mongodb://0.0.0.0/coreshield").then(()=>{
+//     console.log("Database connected")
+//   }).catch((err)=>{
+//     console.log(err)
+//   })
+  
+// }
+
+//atlas
 
 const connectToDb =()=>{
 
     
-  mongoose.connect("mongodb://0.0.0.0/coreshield").then(()=>{
+  mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Database connected")
   }).catch((err)=>{
     console.log(err)
